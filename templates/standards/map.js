7d17fc493ac7da62f81242909367cd33ef2d61dd
joedimarzio <joe.dimarzio@lifeomic.com>
2019-10-03 11:04:58 -0400

    improve scripts and cleanup for sections 1-3

--- templates/standards/map.js
+++ templates/standards/map.js
@@ -1,37 +1,42 @@
-//I wrote this really quickly...I know the namings and time complexities suck
-//I'll fix it later if you guys are interested in using it
-
 const fs = require('fs');
 const lodash = require('lodash');
 
-async function createMappingFields() {
-    var controls = fs.readFileSync("soc2-security.json");
-    controls = JSON.parse(controls);
-    controls = controls['domains'];
 
-    for (domain of controls) {
+async function addStandardToControlsMappings(standardName, controlsMappingFile) {
+    let controlsMappings = JSON.parse(fs.readFileSync(controlsMappingFile));
+
+    for (procedure of controlsMappings['procedures']) {
+        procedure['implements'].push({"standard": `${standardName}`, "requirements": [], "controls": []});
+    }
+
+    let data = JSON.stringify(controlsMappings, null, 2);
+    fs.writeFileSync(controlsMappingFile, data);
+}
+
+async function createMappingFields(standardsDoc, outputFile) {
+    let standards = JSON.parse(fs.readFileSync(standardsDoc));
+
+    for (domain of standards['domains']) {
         for (control of domain['controls']) {
           control['Procedures'] = [];
           control['Status'] = 'Not started';
         }
     }
 
-    let data = JSON.stringify(controls, null, 2);
-    fs.writeFileSync('soc2-mappings.json', data);
+    let data = JSON.stringify(standards, null, 2);
+    fs.writeFileSync(outputFile, data);
 }
 
 
-async function mapProcedures() {
-    var sections = fs.readFileSync("soc2-mappings.json");
-    var mappings = fs.readFileSync("controls-mapping.json");
-    sections = JSON.parse(sections);
-    mappings = JSON.parse(mappings);
+async function mapProcedures(standardsMappingsFile, outputControlsMappingFile) {
+    let standardsMappings = JSON.parse(fs.readFileSync(standardsMappingsFile));
+    let controlsMappings = JSON.parse(fs.readFileSync(outputControlsMappingFile));
 
-    for (section of sections) {
+    for (section of standardsMappings['domains']) {
         for (control of section['controls']) {
             for (procedure of control['Procedures']) {
-                var picked = lodash.filter(mappings['procedures'], { 'id': procedure } )[0];
-                var soc2 = lodash.filter(picked['implements'], { 'standard': 'SOC2' } )[0];
+                let procedureToMap = lodash.filter(controlsMappings['procedures'], { 'id': procedure } )[0];
+                var soc2 = lodash.filter(procedureToMap['implements'], { 'standard': 'SOC2' } )[0];
                 if (soc2['controls'].indexOf(control['ref']) === -1) {
                     soc2['controls'].push(control['ref']);
                 }
@@ -39,9 +44,11 @@ async function mapProcedures() {
         }
     }
 
-    let data = JSON.stringify(mappings, null, 2);
-    fs.writeFileSync('controls-mapping.json', data);
+    let data = JSON.stringify(controlsMappings, null, 2);
+    fs.writeFileSync(outputControlsMappingFile, data);
 
 }
 
-mapProcedures();
+//addStandardToControlsMappings("SOC2", "controls-mapping.json");
+//createMappingFields("soc2.json", "soc2-mappings.json");
+mapProcedures("soc2-mappings.json", "controls-mapping.json");

c4ed8747c6044a1759dae3c2fdbf35caed043435
joedimarzio <joe.dimarzio@lifeomic.com>
2019-10-07 12:46:29 -0400

    check for missing procedures

--- templates/standards/map.js
+++ templates/standards/map.js
@@ -52,17 +52,36 @@ async function mapProcedures(standardsMappingsFile, outputControlsMappingFile) {
 
 async function getUncompletedControls(standardsMappingsFile) {
     let standardsMappings = JSON.parse(fs.readFileSync(standardsMappingsFile));
+    let count = 0;
 
     for (section of standardsMappings['domains']) {
         for (control of section['controls']) {
             if (control['Status'] !== "COMPLETE") {
-                console.log(`Control "${control['ref']}: ${control['summary']}" has status "${control['Status']} "\n`);
+                count += 1;
+                console.log(`Control "${control['ref']}: ${control['summary']}" has status "${control['Status']}"\n`);
             }
         }
     }
+
+    console.log("Total uncompleted controls: " + count);
 }
 
 //addStandardToControlsMappings("SOC2", "controls-mapping.json");
 //createMappingFields("soc2.json", "soc2-mappings.json");
 mapProcedures("soc2-mappings.json", "controls-mapping.json");
 //getUncompletedControls("soc2-mappings.json");
+
+/*
+
+Procedures we should add/modify:
+- BoD charter
+- company organizational chart w/ role descriptions and responsibilities
+- employee expectations (employee evaluations, incentives, performance measures, training, etc)
+- new-hire process
+- communications- internal and external (including info on contracts, NDA, SLA)
+- change (external environment, business, leadership)- how we communicate, process, etc
+- risk (external factors, fraud)
+- physical access logging
+- data transmission
+- vendor expectations
+*/

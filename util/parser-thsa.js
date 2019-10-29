6f6773dd5c7701545d01007670a0b026261e0ba8
erkangz <erkang.zheng@lifeomic.com>
2019-10-29 12:33:35 -0400

    More SOC 2 updates; add vendor-monitor control; add THSA parser and json

---
+++ util/parser-thsa.js
@@ -0,0 +1,69 @@
+/**
+ * This utility parses the SOC 2 controls into a JSON document
+ */
+const csvtojson = require("csvtojson");
+const fs = require('fs');
+const startCase = require('lodash/startcase');
+
+/**
+ * Expected CSV Headers:
+ * Ref,Criteria,Controls,Selected Controls
+ */
+const version = '2019.1';
+const inputFile = `.work/TogetherHealth+Security+Assessment+-+THSA+v2019.1.csv`;
+const outputFile = `../templates/standards/thsa.json`;
+const tempFile = '.work/thsa-controls.json';
+
+const headerRefId = 'SCF #';
+const headerDomain = 'SCF Domain';
+const headerControl = 'SCF Control';
+const headerDescription = 'SCF Control Question';
+
+async function parse() {
+  const framework = {
+    standard: 'THSA',
+    version,
+    webLink: 'https://www.together.health/security-assessment',
+    domains: []
+  };
+
+  const data = await csvtojson().fromFile(inputFile);
+
+  fs.writeFileSync(tempFile, JSON.stringify(data, null, 2));
+
+  const domains = [];
+  const controlsByDomain = {}
+
+  let domain;
+  let controls;
+  for (const control of data || []) {
+    if (control) {
+      domain = control[headerDomain];
+      controls = controlsByDomain[domain] || [];
+  
+      if (!domains.includes(domain)) {
+        domains.push(domain);
+        controlsByDomain[domain] = controls;
+      }
+    
+      if (control[headerRefId].length > 0) {
+        controls.push({
+          ref: control[headerRefId],
+          title: control[headerControl],
+          summary: control[headerDescription]
+        });
+      }
+    }
+  }
+
+  for (const domain of domains) {
+    framework.domains.push({
+      title: domain,
+      controls: controlsByDomain[domain],
+    });
+  }
+
+  fs.writeFileSync(outputFile, JSON.stringify(framework, null, 2));
+}
+
+parse();

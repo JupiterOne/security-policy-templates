78fd72201541783e83061b0e900ec6f19d09f344
erkangz <erkang.zheng@lifeomic.com>
2019-11-01 14:15:55 -0400

    rename

--- util/control-req-mapper.js
+++ util/control-req-mapper.js
@@ -2,22 +2,23 @@
 
 const fs = require('fs');
 
-const hitrustFile = '.work/hitrust-csf-v9.2-all.json';
+const ctrlreqFile = '.work/controls-reqs.json';
 const mappingFile = '../templates/standards/controls-mapping.json';
+const standardName = 'MyStandard'
 
-const hitrust = JSON.parse(fs.readFileSync(hitrustFile)); 
+const ctrlreq = JSON.parse(fs.readFileSync(ctrlreqFile)); 
 
 const mapping = JSON.parse(fs.readFileSync(mappingFile));
 
 const requirementsByControl = {};
 
-for (const domain of hitrust.domains || []) {
+for (const domain of ctrlreq.domains || []) {
   for (const control of domain.controls || []) {
     requirementsByControl[control.ref] = [];
   }
 }
 
-for (const section of hitrust.sections || []) {
+for (const section of ctrlreq.sections || []) {
   for (const requirement of section.requirements || []) {
     if (requirementsByControl[requirement.control]) {
       requirementsByControl[requirement.control].push(requirement.ref);
@@ -27,7 +28,7 @@ for (const section of hitrust.sections || []) {
 
 for (const procedure of mapping.procedures || []) {
   for (const implement of procedure.implements || []) {
-    if (implement.standard === 'HITRUST CSF') {
+    if (implement.standard === standardName) {
       implement.requirements = [];
       for (const control of implement.controls || []) {
         implement.requirements.push(...requirementsByControl[control]);

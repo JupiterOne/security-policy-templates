e24ac7a8fd4b61cdd9fed6d0cc92e841e338e5bb
hesto2 <hestermancarter@gmail.com>
2020-10-14 18:03:39 -0600

    requested changes

--- deploy/terraform/s3.tf
+++ deploy/terraform/s3.tf
@@ -10,8 +10,7 @@ resource "aws_s3_bucket" "security_policy_templates" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
+        sse_algorithm     = "AES256"
       }
     }
   }
